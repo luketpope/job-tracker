61cf925fab96c1a2b9778359189c912a57cb9979
Luke Pope <lukestarpope@gmail.com>
2025-08-01 21:37:49 +0100

    Handle tokens

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,12 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = "your_secret_key_here"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
